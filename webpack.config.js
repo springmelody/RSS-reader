692edca7e24d3f72adf10cbab29e5e9560c6408e
Aleksandr Zakharov <aleksandrzakharov91@gmail.com>
2020-11-27 09:33:24 +0300

    Добавляет npm папки в eslintignore

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
