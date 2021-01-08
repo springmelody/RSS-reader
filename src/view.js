619389907973ba6ce8a592f79c98395944341e28
Aleksandr Zakharov <aleksandrzakharov91@gmail.com>
2021-01-08 19:45:48 +0500

    Удаляет лишние файлы

--- src/view.js
+++ src/view.js
@@ -1,7 +1,5 @@
 import onChange from 'on-change';
 import i18next from 'i18next';
-import renderPosts from './renderPosts.js';
-import renderFeeds from './renderFeeds.js';
 import resources from './locales/en';
 
 const form = document.querySelector('.rss-form');
@@ -14,12 +12,68 @@ i18next.init({
   resources,
 });
 
+const renderFeeds = (feeds) => {
+  const feedsContainer = document.querySelector('.feeds');
+  feedsContainer.innerHTML = '';
+  const feedsTitle = document.createElement('h2');
+  feedsTitle.innerText = i18next.t('feedsTitle');
+  feedsContainer.appendChild(feedsTitle);
+
+  const list = document.createElement('ul');
+  list.setAttribute('class', 'list-group mb-5');
+  feeds.forEach((el) => {
+    const item = document.createElement('li');
+    item.setAttribute('class', 'list-group-item');
+    const itemTitle = document.createElement('h3');
+    itemTitle.innerHTML = el.title;
+    item.appendChild(itemTitle);
+    const itemDescEl = document.createElement('p');
+    itemDescEl.innerHTML = el.description;
+    item.appendChild(itemDescEl);
+    list.appendChild(item);
+    feedsContainer.appendChild(list);
+  });
+};
+
+const renderPosts = (state) => {
+  const postsContainer = document.querySelector('.posts');
+  postsContainer.innerHTML = '';
+  const postsTitle = document.createElement('h2');
+  postsTitle.innerText = i18next.t('postsTitle');
+  postsContainer.appendChild(postsTitle);
+  const postsList = state.rssContent.posts;
+  const listEl = document.createElement('ul');
+  listEl.setAttribute('class', 'list-group');
+  postsList.forEach((post) => {
+    const itemEL = document.createElement('li');
+    itemEL.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-start');
+    const itemElLink = document.createElement('a');
+    itemElLink.setAttribute('href', post.itemLink);
+    if (!state.rssContent.viewedPosts.includes(Number(post.id))) {
+      itemElLink.setAttribute('class', 'font-weight-bold');
+    }
+    itemElLink.setAttribute('data-id', post.id);
+    itemElLink.innerHTML = post.itemTitle;
+    const itemElBtn = document.createElement('button');
+    itemElBtn.setAttribute('class', 'btn btn-primary btn-sm');
+    itemElBtn.setAttribute('data-toggle', 'modal');
+    itemElBtn.setAttribute('data-target', '#modalPreview');
+    itemElBtn.setAttribute('data-id', post.id);
+    itemElBtn.innerHTML = 'Preview';
+    itemEL.appendChild(itemElLink);
+    itemEL.appendChild(itemElBtn);
+    listEl.appendChild(itemEL);
+  });
+  postsContainer.appendChild(listEl);
+};
+
 const errorMessages = {
   required: i18next.t('errorMessages.required'),
   url: i18next.t('errorMessages.url'),
   duplicate: i18next.t('duplicate'),
   network: i18next.t('errorMessages.network'),
 };
+
 const renderViewedPosts = (viewedPosts) => {
   viewedPosts.forEach((id) => {
     const el = document.querySelector(`a[data-id="${id}"]`);
@@ -60,6 +114,7 @@ const renderSuccessMessage = () => {
   feedbackContainer.classList.add('text-success');
   feedbackContainer.innerHTML = i18next.t('loaded');
 };
+
 const handleProcessState = (watchedState) => {
   if (watchedState.form.processState === 'empty' || watchedState.form.processState === 'filling') {
     submitButton.disabled = false;
@@ -85,20 +140,23 @@ const handleProcessState = (watchedState) => {
 
 const render = (state) => onChange(state, (path) => {
   const watchedState = render(state);
-  if (path === 'form.processState') {
-    handleProcessState(watchedState);
-  }
-
-  if (path === 'rssContent.posts') {
-    renderPosts(watchedState);
-  }
-
-  if (path === 'rssContent.feeds') {
-    renderFeeds(watchedState.rssContent.feeds);
-  }
-
-  if (path === 'rssContent.viewedPosts') {
-    renderViewedPosts(watchedState.rssContent.viewedPosts);
+  switch (path) {
+    case 'form.processState':
+      handleProcessState(watchedState);
+      break;
+    case 'rssContent.posts':
+      renderPosts(watchedState);
+      break;
+    case 'rssContent.feeds':
+      renderFeeds(watchedState.rssContent.feeds);
+      break;
+    case 'rssContent.viewedPosts':
+      renderViewedPosts(watchedState.rssContent.viewedPosts);
+      break;
+    default:
+      console.log(`Unknown path: ${path}`);
+      break;
+      // throw new Error(`Unknown path: ${path}`);
   }
 });
 
